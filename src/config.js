7f1c0715007ce6903ea41daf0f718520efc4c81c
Damididy <shanyin18@gmail.com>
2025-12-25 16:43:40 +0800

    config: set production api url to render

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.PROD
+    ? 'https://gym-anti-backend.onrender.com'
+    : 'http://localhost:3000';
+
 export default API_BASE_URL;
